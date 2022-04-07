4066d8917de96f7e2d94d24f587723ca30754c7d
Dario Camargo <dpcamargo@gmail.com>
2022-04-06 22:09:38 -0300

    fixed social icons and project floating text position and background color.

--- assets/js/code.js
+++ assets/js/code.js
@@ -1,14 +1,39 @@
-let projectContainers = document.getElementsByClassName('project_container')
-let ProjecsLength = document.getElementsByClassName("project_container").length
+let projectContainers = document.getElementsByClassName("project_container");
+let ProjecsLength = document.getElementsByClassName("project_container").length;
+let scrool = true;
 
-ProjecsLength.map
+function preventScroll(e) {
+  e.preventDefault();
+  e.stopPropagation();
+
+  return false;
+}
+
+function disableScrool() {
+  document
+    .getElementById("wrapper")
+    .addEventListener("wheel", preventScroll);
+}
+
+function enableScrool() {
+  document
+    .getElementById("wrapper")
+    .removeEventListener("wheel", preventScroll);
+}
 
 for (let i = 0; i < ProjecsLength; i++) {
-    let element = projectContainers[i]
-    element.addEventListener('click', () => {
-        let src = element.getAttribute('project-src')
-        document.getElementById('code_modal_iframe').src = src
-        document.getElementById('code_modal').classList.add('visible')
-        document.getElementById('code_modal').classList.add('loaded')
-    })
+  let element = projectContainers[i];
+  element.addEventListener("click", () => {
+    let src = element.getAttribute("project-src");
+    document.getElementById("code_modal_iframe").src = src;
+    document.getElementById("code_modal").classList.add("visible");
+    document.getElementById("code_modal").classList.add("loaded");
+
+    document.querySelector("iframe").style.overflow = "auto";
+    disableScrool()
+  });
 }
+
+document.getElementById("close").addEventListener("click", () => {
+    enableScrool()
+});
